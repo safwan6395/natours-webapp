66fa97f63d5031a51ce4baa743aa024dda7fdd4b
Safwan Hassan <safwan.hassan.d@gmail.com>
2023-08-02 15:42:06 +0500

    Build the template to display each individual tour

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -8,15 +8,21 @@ exports.getOverview = catchAsync(async (req, res) => {
   // 2. Build the template
 
   // 3. Render the template
-
   res.status(200).render('overview', {
-    title: 'All Tours',
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate(
+    {
+      path: 'reviews',
+      fields: 'review rating user',
+    }
+  );
+
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: tour.name,
+    tour: tour,
   });
-};
+});
